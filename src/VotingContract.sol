f73428d6004776e52ad7d692aeca4fae35d3d6b6
Tomas Kosub <tomas.kosub@gmail.com>
2024-02-26 19:09:06 +0100

    contract changes

--- src/VotingContract.sol
+++ src/VotingContract.sol
@@ -13,6 +13,7 @@ contract VotingContract {
     // #############################
 
     error VotingContract__CandidateNotWhitelisted();
+    error VotingContract__IsNotTheOwner();
 
     // #############################
     //           EVENTS
@@ -22,18 +23,75 @@ contract VotingContract {
     event CandidateRegistered(uint256 indexed candidateId, address candidateIdentifier);
 
     // #############################
+    //           EVENTS
+    // #############################
+
+    modifier onlyOwner() {
+        if (msg.sender != s_owner) {
+            revert VotingContract__IsNotTheOwner();
+        }
+        _;
+    } 
+
+    // #############################
     //      STATIC VARIABLES
     // #############################
 
+    enum RegisteredParties {
+        LEFT,
+        RIGHT,
+        FAR_RIGHT,
+        FAR_LEFT,
+        CENTRIST
+    }
+
     struct Candidate {
         bytes32 name;
-        bytes32 district;
-        bytes32 politicalAffiliation;
+        RegisteredParties affiliation;
         address identifier;
         uint8 age;
-        uint8 id;
+        uint8 number;
     }
 
     Candidate[] internal candidates;
+
+    uint8 public constant MAXIMUM_CANDIDATES = 6;
+    address private s_owner;
+
+    // #############################
+    //        FUNCTIONS
+    // #############################
+
+    constructor() {
+        s_owner = msg.sender;
+    }
+
+    // #############################
+    //       EXTERNAL FUNCTIONS
+    // #############################
+
+    /// @notice This function registeres a new candidate that is whitelisted, if the total number of candidates is not yet reached
+    /// @param candidateName a name of the candidate
+    /// @param candidateAffiliation political possition of the candidate
+    /// @param candidateId address of the candidate address
+    /// @param candidateAge age of the candidate
+    function registerCandidate(bytes32 candidateName, RegisteredParties candidateAffiliation, address candidateId, uint8 candidateAge) external returns (uint8 candidateNumber) {
+        // Cannot register if the maximum number of candidates way reached
+        // Cannot register with incorrect political affiliation
+        // Cannot register if the age is below 18
+
+    }
+
+    function castVote() external {}
+
+    function openVoting() external {}
+
+    function remainingTime() external returns (uint256) {}
+
+    function closeRegistration() external {}
+
+    function getResults() external {}
+
+    function _whitelistCandidate() internal {}
 
 }
